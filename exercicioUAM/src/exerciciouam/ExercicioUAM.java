5abdaa36e4231a9016ca28074a5c092ed169ec44
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2022-09-02 21:23:04 -0300

    Ex02/09

---
+++ exercicioUAM/src/exerciciouam/ExercicioUAM.java
@@ -0,0 +1,76 @@
+
+package exerciciouam;
+import java.util.Scanner;
+
+public class ExercicioUAM {
+    public static void main(String[] args) {
+      Scanner entrada = new Scanner (System.in);
+              
+//        //Ler inteiro e printar na tela
+//        
+//        int numero=5; 
+//        System.out.println("Digite um numero inteiro: ");
+//        numero = entrada.nextInt();
+//        System.out.println("O numero digitado é: " + numero);
+//        entrada.close();
+          
+//        //Faça um numero que leia o numero real
+//        double numero_real;
+//        System.out.println("Digite um numero real: ");
+//        numero_real = entrada.nextDouble();
+//        System.out.println("O numero digitado é: " +numero_real);
+       
+//         //Peça ao usuario para digitar tres numeros inteiro 
+//         int numero1, numero2, numero3, soma;
+//         System.out.println("Digite um numero inteiro:");
+//         numero1 = entrada.nextInt();
+//         System.out.println("Digite outro numero inteiro:");
+//         numero2 = entrada.nextInt();
+//         System.out.println("Digite outro numero inteiro:");
+//         numero3 = entrada.nextInt();
+//         soma=numero1+numero2+numero3;
+//         System.out.println("A soma dos numeros:" + soma);
+        
+//        //Tranforme °C em °F
+//        float fire, celsius;
+//        System.out.println("Digite a temperatura em °C:");
+//        celsius = entrada.nextFloat();
+//        fire = celsius * (float)(9.0 / 5.0) + 32;
+//        System.out.printf("A temperatura é: %.2f", fire);
+//        entrada.close();
+               
+//          //Faça a transição de km/h por m/s
+//          double velocidade_km, velocidade_ms;
+//          System.out.println("Digite a velocidade em Km/H:");
+//          velocidade_km = entrada.nextDouble();
+//          velocidade_ms = velocidade_km / 3.6;
+//          System.out.println("Velocidade em metros por segundo:" + velocidade_ms + "m/s");
+//          entrada.close();
+          
+//          //Imprima o antecessor do numero que o usuario colocar
+//          int numero, antecessor;
+//          System.out.println("Digite um número inteiro");
+//          numero = entrada.nextInt();
+//          antecessor = numero - 1;
+//          System.out.println("O antecessor do número é:" + antecessor);
+
+//          //Calcule a idade do usuario apartir do ano de nascimento
+//          int ano_nascimento, idade;
+//          int ano = 2022;
+//          System.out.println("Digite o seu ano de nascimento");
+//          ano_nascimento = entrada.nextInt();
+//          idade = ano - ano_nascimento;
+//          System.out.println("A sua idade é:" + idade);
+            
+//          //Calcule a area da circuferencia
+//          float pi = (float) 3.141415926535;
+//          float raio = 2*2;
+//          float area = pi * raio;
+//          System.out.printf("A área da circunferência é: %.2f \n", area);
+
+        
+        
+
+    }
+    
+}
