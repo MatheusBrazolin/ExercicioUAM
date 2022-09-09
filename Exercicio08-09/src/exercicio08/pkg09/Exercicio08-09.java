4046a2420bcfeca30230cd6036126263864d82c7
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2022-09-08 21:31:24 -0300

    Aula UAM (08-09-2022)

---
+++ Exercicio08-09/src/exercicio08/pkg09/Exercicio08-09.java
@@ -0,0 +1,37 @@
+
+package exercicio08.pkg09;
+import java.util.Scanner;
+
+public class Exercicio0809 {
+    public static void main(String[] args) {
+//        Scanner entrada = new Scanner (System.in); 
+//        int numeros[]=new int[10];
+//        for(int i = 0; i <10; i++) {
+//            System.out.println("Digite o" + (i+i)+ "0° valor");
+//            numeros[i]=entrada.nextInt();
+//    }
+//        for (int i = 9; i>=0;i--){
+//        System.out.println(numeros[i]);
+//        }    
+
+//            Scanner entrada = new Scanner (System.in); 
+//            int vetorA[]=new int[8];
+//            int vetorB[]=new int[8];
+//            for(int i = 0; i <8; i++) {
+//            System.out.println("Digite um valor:");
+//            vetorA[i]=entrada.nextInt();
+//        }
+//        for(int i=0;i<8; i++ ){
+//            vetorB[i]=vetorA[i]*3;
+//    }
+//        for (int i = 0; i<8; i++){
+//            System.out.println(vetorB[i]);
+//    }  
+
+     
+    }
+}   
+
+
+
+
