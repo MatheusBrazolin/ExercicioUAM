e83c8e904fcccffdf8ad698692f1baedd880edce
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2022-09-09 21:22:51 -0300

    Aula 09-09

---
+++ Exercicios09-09/src/exercicios09/pkg09/Exercicios0909.java
@@ -0,0 +1,49 @@
+
+package exercicios09.pkg09;
+import java.util.Scanner;
+public class Exercicios0909 {
+
+    public static void main(String[] args) {
+       
+        //Divisão de 2 numeros
+        
+//       Scanner teclado = new Scanner (System.in);
+//       double numero1 = 0;
+//       double numero2 = 0;       
+//       System.out.println("Digite um numero:");
+//       numero1 = teclado.nextDouble(); 
+//       
+//       System.out.println("Digite outro numero:");
+//       numero2 = teclado.nextDouble();
+//       
+//       System.out.println("Numero1 / numero2 = " + (numero1/numero2));         
+        
+
+        ///Divida dois numeros e ache o resto da divisão 
+//        //declaração das variaveis
+//        Scanner teclado = new Scanner (System.in);
+//        double numero1;
+//        double numero2;  
+//        double resto;
+//        double divisao;
+//        
+//        //Entrada de dados
+//        System.out.println("Digite um numero:");
+//        numero1 = teclado.nextDouble(); 
+//        System.out.println("Digite outro numero:");
+//        numero2 = teclado.nextDouble();
+//       
+//        //Fazer as contas
+//        divisao = numero1 / numero2;
+//        resto = numero1 - numero2 * (int)divisao;
+//        
+//        //saida
+//        System.out.println(resto); 
+//        
+//        teclado.close();
+
+
+
+
+    } 
+}
