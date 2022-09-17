ac3d9ee32981e90739aeb9e5a87f68ec37c5692b
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2022-09-16 21:32:49 -0300

    Exercicio For 16-09

---
+++ ExFor16-09/src/exfor16/pkg09/ExFor1609.java
@@ -0,0 +1,57 @@
+
+package exfor16.pkg09;
+import java.util.Scanner;
+
+public class ExFor1609 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        /// Ecrever um programa de escreva de 1 a 50
+//    for (int i = 0; i <=50; i++ ){ 
+//        System.out.println(i);
+//    }
+    
+        ///Crie um programa que calcule um fetorial de um numero, mas deve ser regressivo. 
+//        int  acumulador = 1,  fatorial;
+
+//            System.out.println("digite um numero para calculo fatorial");
+//              fatorial = teclado.nextInt();
+//              for(int i = fatorial; i> 0; i--) {
+//                  fatorial = i * (fatorial - 1);
+//                                 acumulador *= i ; 
+//               } 
+//                  System.out.println("");
+//                  
+//        }
+        
+        
+        /// Ecrever um programa que escreva de 1 a 100 mas somente os numeros pares. 
+//        
+//        for (int i = 1; i <=50; i++ ){ 
+//            System.out.println(i*2);
+//    }
+    
+///           EX4
+
+//            int  numero  ;
+//            System.out.println( " Insira o numero que voce quer multiplicar " );
+//            int numero = teclado.nextInt();
+//            for ( int  i  =  1 ; i  <=  10 ; i ++ ){
+//                int  resultado  =  (numero  *  i) ;
+//                System.out.println(resultado);
+
+///           EX5
+
+//            System.out.println("Digite quantas idades vc quer usar:");
+//            int totalDeIdade = teclado.nextInt();
+//            int idade[] = new int [totalDeIdade];
+//            for (int i = 0; i<= idade.length; i ++) {
+//                System.out.println("Digite a idade");
+//                idade[i]=teclado.nextInt();
+//                System.out.println(idade[i]);
+//            }
+//            for(int i = 0; i<=idade.length; i++){
+//               int soma = idade[i]/idade.length;
+//                System.out.println(soma);
+//            }
+    }
+}
